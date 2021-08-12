64fc1599814d037019e0a24317c8c59b9f4a0044
Suyai Slootmans <suyaislootmans@gmail.com>
2021-08-12 17:34:15 -0300

    Dismissible

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -41,20 +41,38 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  ListTile _bandTile(Band band) {
-    return ListTile(
-      leading: CircleAvatar(
-        child: Text(band.name.substring(0, 2)),
-        backgroundColor: Colors.blue.shade100,
-      ),
-      title: Text(band.name),
-      trailing: Text(
-        '${band.votes}',
-        style: TextStyle(fontSize: 20),
-      ),
-      onTap: () {
-        print(band.name);
+  Widget _bandTile(Band band) {
+    return Dismissible(
+      key: Key(band.id),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (DismissDirection direction) {
+        print('direction: $direction');
+        print('id: ${band.id}');
       },
+      background: Container(
+        padding: EdgeInsets.only(left: 8.0),
+        color: Colors.red.withOpacity(0.8),
+        child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Delete Band',
+              style: TextStyle(color: Colors.white),
+            )),
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Text(band.name.substring(0, 2)),
+          backgroundColor: Colors.blue.shade100,
+        ),
+        title: Text(band.name),
+        trailing: Text(
+          '${band.votes}',
+          style: TextStyle(fontSize: 20),
+        ),
+        onTap: () {
+          print(band.name);
+        },
+      ),
     );
   }
 
