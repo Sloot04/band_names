ff5daf6394204b9a76238dbeeb3a6635849178e6
Suyai Slootmans <suyaislootmans@gmail.com>
2021-08-17 13:23:15 -0300

    Votar por una banda

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -67,6 +67,8 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _bandTile(Band band) {
+    final socketService = Provider.of<SocketService>(context, listen: false);
+
     return Dismissible(
       key: Key(band.id),
       direction: DismissDirection.startToEnd,
@@ -95,7 +97,7 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(fontSize: 20),
         ),
         onTap: () {
-          print(band.name);
+          socketService.socket.emit('vote-band', {'id': band.id});
         },
       ),
     );
